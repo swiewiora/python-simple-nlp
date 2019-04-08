c3e17a669c899d2682d98b8f32704573f69165b1
swiewiora <>
2019-04-08 21:16:06 +0200

    Added license

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r") as readme:
+with open('README.md', 'r') as readme:
     long_description = readme.read()
 
 setuptools.setup(
@@ -8,10 +8,13 @@ setuptools.setup(
     version='1.0',
     zip_safe=False,
     packages=setuptools.find_packages(),
-    author="Sebastian Wiewiora",
-    author_email="sebawiewior@gmail.com",
-    description="Simple NLP program",
+    author='Sebastian Wiewiora',
+    author_email='sebawiewior@gmail.com',
+    description='Python program which converts sequence of sentences in '
+                'natural language and constant w into one or more '
+                'character arrays of length w',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/swiewiora/python-simple-nlp",
+    long_description_content_type='text/markdown',
+    url='https://github.com/swiewiora/python-simple-nlp',
+    license='MIT',
  )
